f06fa87a5b9a4aaef9ab0f4b836c6b04fa71597b
Michael Adam <obnox@samba.org>
2026-02-03 19:00:46 +0100

    images: improve fedora-c image build
    Signed-off-by: Michael Adam <obnox@samba.org>

--- install-packages_fedora_c.sh
+++ install-packages_fedora_c.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+		set -euo pipefail
 
-dnf install -y groupinstall
-dnf -y groupinstall "Development Tools"
-dnf install  -y git gcc make cmake automake autoconf vim
-dnf install -y python3
 
+		dnf group install -y development-tools c-development && \
+		dnf install  -y git gcc make cmake automake autoconf vim less python3
+
+		RC=$?
+
+		if [ ${RC} -eq 0 ]; then
+				echo "successfully installed packages."
+		else
+				echo "error installing packages."
+        fi
+
+		exit ${RC}
 
