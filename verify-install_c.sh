c781488091885362c45fc5f1e9a77e205b08fd3a
Michael Adam <obnox@samba.org>
2026-02-03 12:58:44 +0100

    images: add verification to fedora-c images
    Signed-off-by: Michael Adam <obnox@samba.org>

---
+++ verify-install_c.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+		SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+		source "${SCRIPTDIR}/common.sh"
+
+		echo "verifying c install."
+
+		echo "1. verifying that make is installed."
+
+		verify_binary "make" || exit 1
+
+		echo "2. veigying that gcc is installed."
+		verify_binary "gcc" || exit 1
+
+
+echo "2. verifying working of commands"
+
+make --version && gcc --version
